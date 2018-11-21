264ed19b569180951e31d3610a587f4f8977e19e
Maofkasopfkcafopasf <mohamedpadr94@gmail.com>
2018-11-21 22:31:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,18 +22,6 @@ client.on('message', msg =>{
   }
 })
 
-client.on('message', message => {
-    if(message.content === 'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === 'كريدتس'){
-        message.channel.send('#credits')
-    }
-});
-
 client2.on('message', msg =>{
         let args = msg.content.slice(4);
   if (msg.author.id !== '316324088865882142') return;
@@ -42,17 +30,6 @@ client2.on('message', msg =>{
   }
 })
 
-client2.on('message', message => {
-    if(message.content === 'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === 'كريدتس'){
-        message.channel.send('#credits')
-    }
-});
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
@@ -83,6 +60,54 @@ if (message.content === '!spam') {
 });
 
 
+var ServerID = "509375429413699585";
+var ChannelID = "514413581064994816";
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By End','\n','ProBot credits miner is ready!'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+    client.on('ready', () => {
+    function timerFunc() {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) 
+    }
+
+    var timer = setInterval(timerFunc, 1000);
+
+
+    });
+
+
+
+client2.on('warn', console.warn);
+
+client2.on('error', console.error);
+
+
+client2.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By End','\n','ProBot credits miner is ready!'));
+
+client2.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client2.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+    client2.on('ready', () => {
+    function timerFunc() {
+        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) 
+    }
+
+    var timer = setInterval(timerFunc, 1000);
+
+
+    });
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
